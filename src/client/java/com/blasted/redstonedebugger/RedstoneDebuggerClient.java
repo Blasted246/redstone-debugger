2b6a1f39b8274f6955218de56dae3cb778763215
Blasted <jamesraine2008@icloud.com>
2025-03-20 00:49:12 -0700

    Initial Commit
    Added files straight from fabric generator so that I can come back if I mess something up

---
+++ src/client/java/com/blasted/redstonedebugger/RedstoneDebuggerClient.java
@@ -0,0 +1,10 @@
+package com.blasted.redstonedebugger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedstoneDebuggerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/blasted/redstonedebugger/RedstoneDebuggerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.blasted.redstonedebugger;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RedstoneDebuggerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
